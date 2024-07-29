a1f2b8b7846612edc5893132b1a70735ee293c19
jfletcher5 <jon.d.fletch@gmail.com>
2024-07-29 11:25:32 -0400

    jon with audio

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.llastmile.tourguidepoc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/index.dart
+++
@@ -1,4 +0,0 @@
-// Export pages
-export '/signin/signin_widget.dart' show SigninWidget;
-export '/landing_page/landing_page_widget.dart' show LandingPageWidget;
-export '/live_tour/live_tour_widget.dart' show LiveTourWidget;
